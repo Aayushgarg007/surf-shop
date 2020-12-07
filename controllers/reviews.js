f9f4448be99f21ddd665e3f163da2b6066decd76
Aayushgarg007 <aayushgarg226@gmail.com>
2020-12-08 03:55:39 +0530

    restrict reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -5,15 +5,22 @@ module.exports = {
   // Reviews Create
   async reviewCreate(req, res, next) {
     // find the post by id
-    let post = await Post.findById(req.params.id);
+    let post = await Post.findById(req.params.id).populate('reviews').exec();
+    // here we can restricting each user with only one review
+    let haveReviewed = post.reviews.filter(review => {
+      return review.author.equals(req.user._id);
+    }).length;
+    if(haveReviewed) {
+      req.session.error = 'Sorry you can only create one review per post.';
+      return res.redirect(`/posts/${post.id}`);
+    }
     // creat a review
     req.body.review.author = req.user._id;
-    let review = new Review(req.body.review);
+    let review = new Review.create(req.body.review);
     // assign review to post
     post.reviews.push(review);
-    // save the post and review
-    review.save();
-    post.save();
+    // save the post
+    await post.save();
     // redirect to the post
     req.session.success = 'Review created successfully';
     res.redirect(`/posts/${post.id}`);
@@ -31,6 +38,7 @@ module.exports = {
     await Post.findByIdAndUpdate(req.params.id, {
       $pull: { reviews: req.params.review_id }
     });
+    // $pull is part of mongoDB to delete items from an array in DB
     await Review.findByIdAndRemove(req.params.review_id);
     req.session.success = "Review deleted successfully";
     res.redirect(`/posts/${req.params.id}`);
